52720c08d9eb41fb3e165d1b63762888d7a3335c
ashok05253002 <ashokkumarm25052002@gmail.com>
2025-04-28 18:56:31 +0530

    / fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
-  base: './',
+  base: '/Pro-Media1/', // <-- CHANGE this to your GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
-  }
+  },
 });
